225b93e32cef8e7003bdf6c9968d8ef4893031e5
Gil Pratte <gil.pratte@mutualmobile.com>
2016-07-21 21:21:09 -0500

    comment out the references to types

--- BasePokemon.java
+++ BasePokemon.java
@@ -2,6 +2,7 @@
 public abstract class BasePokemon {
 
 	protected String name;
+	// protected String[] types; // e.g. Water, Fire, ...
 	protected int attack;
 	protected int defense;
 
@@ -12,6 +13,13 @@ public abstract class BasePokemon {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	// public String[] getTypes() {
+	// 	return types;
+	// }
+	// public void setTypes(String[] types) {
+	// 	this.types = types;
+	// }
 
 	public int getAttack() {
 		return attack;
@@ -37,6 +45,11 @@ public abstract class BasePokemon {
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
 		sb.append("name=" + name);
+		// if (types != null) {
+		// 	for (String type : types) {
+		// 		sb.append(", type=" + type);
+		// 	}
+		// }
 		sb.append(", attack=" + attack).append(", defense=" + defense);
 		return sb.toString();
 	}
